e6863629ba0596466f3fce6eda084aadfa0a0252
simsosimso <simsamsom33@gnu.ac.kr>
2022-10-19 17:04:18 +0900

    web build

---
+++ build/modal_progress_hud_nsn/generated/source/buildConfig/debug/com/walle/modal_progress_hud_nsn/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.walle.modal_progress_hud_nsn;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.walle.modal_progress_hud_nsn";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
