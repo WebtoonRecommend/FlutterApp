43583c9273c776b46eccc00b833aa8a7d9043265
simsosimso <simsamsom33@gnu.ac.kr>
2022-11-23 01:40:36 +0900

    사용하지 않는 페이지 삭제

--- lib/presentation/detailtwo_screen/binding/detailtwo_binding.dart
+++
@@ -1,9 +0,0 @@
-import '../controller/detailtwo_controller.dart';
-import 'package:get/get.dart';
-
-class DetailtwoBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => DetailtwoController());
-  }
-}
